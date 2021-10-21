3710affe34c7409df84a8f25b21af0ecb919d64b
Alex Shoykhet <alex.shoykhet@coinbase.com>
2021-10-21 00:36:35 -0700

    Remove code unrelated to crash

--- tutorials/getting-started-v2/background.js
+++ tutorials/getting-started-v2/background.js
@@ -1,9 +1,4 @@
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-
   chrome.windows.create(
     {
      url: chrome.runtime.getURL("popup.html"),
--- tutorials/getting-started/background.js
+++ tutorials/getting-started/background.js
@@ -1,12 +1,5 @@
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
-
-
-chrome.tabs.onRemoved.addListener((tabId) => {
-});
+/**
+ *
+ */
 chrome.windows.onRemoved.addListener((winId) => {
 });
